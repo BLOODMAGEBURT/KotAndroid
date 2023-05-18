ba885fa0633db0728a29060e28a0867eb572f6f8
xujianbo <xujianbovip@gmail.com>
2023-05-18 16:04:30 +0800

    file choose

--- app/src/main/java/com/xu/kotandroid/ui/FileChooseActivity.kt
+++ app/src/main/java/com/xu/kotandroid/ui/FileChooseActivity.kt
@@ -1,10 +1,14 @@
 package com.xu.kotandroid.ui
 
 import android.annotation.SuppressLint
-import android.content.ContentResolver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import coil.load
 import com.xu.kotandroid.R
 import com.xu.kotandroid.base.BaseActivity
 import com.xu.kotandroid.databinding.ActivityFileChooseBinding
@@ -20,9 +24,33 @@ class FileChooseActivity : BaseActivity<ActivityFileChooseBinding>(R.layout.acti
         }
     }
 
+    private val pickMedia =
+        registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+            // Callback is invoked after the user selects a media item or closes the
+            // photo picker.
+            if (uri != null) {
+                Log.d("PhotoPicker", "Selected URI: $uri")
+
+                binding.iv.load(uri)
+            } else {
+                Log.d("PhotoPicker", "No media selected")
+            }
+        }
+
+
+    private val pickMultipleMedia =
+        registerForActivityResult(ActivityResultContracts.PickMultipleVisualMedia(5)) { uris ->
+            // Callback is invoked after the user selects media items or closes the
+            // photo picker.
+            if (uris.isNotEmpty()) {
+                Log.d("PhotoPicker", "Number of items selected: ${uris.size}")
+            } else {
+                Log.d("PhotoPicker", "No media selected")
+            }
+        }
+
 
     override fun initData() {
-
 
     }
 
@@ -31,6 +59,21 @@ class FileChooseActivity : BaseActivity<ActivityFileChooseBinding>(R.layout.acti
             resultLauncher.launch(true)
         }
 
+        binding.choosePic.setOnClickListener {
+
+            if (ActivityResultContracts.PickVisualMedia.isPhotoPickerAvailable(this)) {
+                Log.d("PhotoPicker", "Preview is supported")
+
+                pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+            } else {
+                Log.d("PhotoPicker", "Preview is not supported")
+                Toast.makeText(this, "不支持图片选择器", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.chooseMultiPic.setOnClickListener {
+            pickMultipleMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
 
     }
 
